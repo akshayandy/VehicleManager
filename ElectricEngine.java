2d6a4e870790abb4578791ae0b5f2727c24e290f
akshayandy847 <akshayandy847@gmail.com>
2023-03-10 19:44:03 +0530

    Electric generate power and stop added

---
+++ ElectricEngine.java
@@ -0,0 +1,14 @@
+
+public class ElectricEngine {
+	public void start() {
+		System.out.println("Electric vehicle started..!");
+	}
+
+	public void stop(){
+		System.out.println("Electric vehicle stopped");
+	}
+
+	public void generatePower(){
+		System.out.println("Electrical generating power")
+	}
+}
---
+++ HydrogenEngine.java
@@ -0,0 +1,8 @@
+public class HydrogenEngine {
+    public void start() {
+        System.out.println("Hydrogen Vehicle Started");
+    }
+    public void stop() {
+        System.out.println("Hydrogen Vehicle Stopped");
+    }
+}
